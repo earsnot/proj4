a438a91674f47eae08d41a4f0f4d0bbe65dd1954
earsnot <sjlund@gmail.com>
2019-11-21 16:13:44 +0100

    added comments to drivemodes, added constants.py

--- modules/drivemodes.py
+++ modules/drivemodes.py
@@ -1,22 +1,39 @@
+LOWEST_THRESHOLD = 20
+
 class ModeSelector:
-    drivemode = 0
-    def get_drivemode(self):
+    drivemode = 0 #initializing drivemode variable
+
+    def get_drivemode(self): #method to return current drivemode
         return self.drivemode
 
     def eco_mode(self):
-        self.drivemode = 0
-        return self.drivemode
+        #set_pin(129) #set controlsignal pin
+        self.drivemode = 0 
+        return self.drivemode #return current drivemode
 
     def normal_mode(self):
+        #set_pin #set control signal pin
         self.drivemode = 1
-        return self.drivemode
+        return self.drivemode  #return current drivemode
 
     def sport_mode(self):
+        #set_pin #set control signal pin
         self.drivemode = 2
-        return self.drivemode
+        return self.drivemode #return current drivemode
+
+    def SOC_based_mode_select(self, soc):
+        if soc < LOWEST_THRESHOLD: #compare SOC to const LOWEST_THRESHOLD
+            self.eco_mode()
+            print("is sustainable")
+
+    
 
 
 # test
+
+ms = ModeSelector()
+ms.SOC_based_mode_select(15)
+
 
 #mode = DriveModeHandler()
 #mode.normal_mode()
