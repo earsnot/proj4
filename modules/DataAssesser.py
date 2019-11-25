3e072179a4e58efd76287619f7b6a92c11eee525
TroelsChr <57835899+TroelsChr@users.noreply.github.com>
2019-11-25 11:47:29 +0100

    Added: DataAssesser.py (not complete)

---
+++ modules/DataAssesser.py
@@ -0,0 +1,13 @@
+class DataAssesser:
+	"""docstring for DataAssesser"""
+	def __init__(self, sensor_value , sensor_type):
+		self.sensor_value = sensor_value
+
+	def check_sensor_type(self):
+		
+
+	def check_sensor_readings(self):
+		if self.sensor_value > OR 
+		
+	def check_if_SOC_too_low(self):
+		pass
