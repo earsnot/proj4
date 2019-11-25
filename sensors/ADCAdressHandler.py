3846e5d16d989f2689536e8f4883cf4fe361528f
TroelsChr <57835899+TroelsChr@users.noreply.github.com>
2019-11-25 10:52:44 +0100

    Added: ADCAdressHandler.py (missing real constant imports)

--- sensors/ADCAdressHandler.py
+++ sensors/ADCAdressHandler.py
@@ -1,8 +1,18 @@
+# from constants import *
+# import adresses from constant (LINE 1 & 10-12)
+
 class ADCAdressHandler:
 	"""docstring for """
-	def __init__(self, adress_input):
-		self.adress_input = adress_input
+	def __init__(self, adress_selection_value):
+		self.adress_selection_value = adress_selection_value
 
-	def adress_return(self):
-		return self.adress_input
-		
+	def set_adress(self):
+		# adress1 = import adress1
+		# adress2 = import adress2
+		# adress3 = import adress3
+		if self.adress_selection_value == 0:
+			return adress1
+		elif self.adress_selection_value == 1:
+			return adress2
+		elif self.adress_selection_value == 2:
+			return adress3
