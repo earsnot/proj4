3c209bd74d23557a3e19fed0859a0d540db75299
TroelsChr <57835899+TroelsChr@users.noreply.github.com>
2019-11-26 10:19:22 +0100

    DataAssesser and constants updated

--- modules/DataAssesser.py
+++ modules/DataAssesser.py
@@ -1,13 +1,22 @@
+from constants import *
+
 class DataAssesser:
 	"""docstring for DataAssesser"""
-	def __init__(self, sensor_value , sensor_type):
-		self.sensor_value = sensor_value
-
-	def check_sensor_type(self):
-		
+	def __init__(self, sensorValue , sensorType):
+		self.sensorValue = sensorValue
+		self.sensorType = sensorType
 
 	def check_sensor_readings(self):
-		if self.sensor_value > OR 
+		if self.sensorType == 0:
+			if self.sensorValue < MIN_TEMP_SENSOR or self.sensorValue > MAX_TEMP_SENSOR:
+				return 0
+		elif self.sensorType == 1:
+			if self.sensorValue < MIN_TEMP_SENSOR or self.sensorValue > MAX_CURENT_SENSOR:
+				return 1
+		elif self.sensorType == 2:
+			if self.sensorValue < MIN_VOLT_SENSOR or self.sensorValue > MAX_VOLT_SENSOR:
+				return 2
 		
-	def check_if_SOC_too_low(self):
-		pass
+
+A = DataAssesser(34,2)
+print(A.check_sensor_readings())
