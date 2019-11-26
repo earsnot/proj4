9c67ed3931d74d46eb4044c9411a10b60a0466a4
TroelsChr <57835899+TroelsChr@users.noreply.github.com>
2019-11-26 10:40:01 +0100

    updated constants

--- libs/constants.py
+++ libs/constants.py
@@ -1,24 +1,19 @@
-<<<<<<< HEAD
 # holding constants
-LOWEST_THRESHOLD = 20
-ADC_VOLTAGE = 3.3
+LOWEST_THRESHOLD = 20 # [%] 
+ADC_VOLTAGE = 3.3 # [Volt] 
 # adc pins
-ADC_VOLTAGE = 'X19'
+ADC_VOLTAGE_PIN = 'X19'
 ADC_CURRENT = 'X20'
 ADC_TEMPERATURE = 'X21'
 # pins for drive mode selection
 
-=======
 #holding constants
 BATTERY_CAPACITY = 18 # [Ah] (Amp-hours)
-LOWEST_SOC_THRESHOLD = 0.2
 MIN_BATTERY_VOLTAGE = 42 # [Volts]
 MAX_BATTERY_VOLTAGE = 52 # [Volts]
 BATTERY_RESISTANCE = 0.06 # [Ohms]
 R_DIS_DT_10S = 0.28 # [Ohms] (3.7V / 13.2A) - 10 second voltage drop test - From ballpark test. Resistance calculated from voltage drop over 10 seconds divided by current draw.
-ADC_VOLTAGE = 3.3 # [Volts]
 ECO_PIN = 'x1'
->>>>>>> 90bfb37f2f8601bf5e87178feb1403af0b9927f6
 NORMAL_PIN = 'x2'
 SPORT_PIN = 'x3'
 
