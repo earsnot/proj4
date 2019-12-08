e842eac7582a6f29779958d61e902cdad06799c5
MortMort <Martin.therkildsen@gmail.com>
2019-12-08 10:23:50 +0100

    soc reworked

--- pybflash/modules/soc.py
+++ pybflash/modules/soc.py
@@ -1,28 +1,67 @@
-class SOC:
+# Test includes:
+from libs.constants import *
+
+# Test voltage limits:
+V1 = 40
+V2 = 43
+V3 = 45
+V4 = 49
+V5 = 54
+
+
+class SOCOCV:
 	"""Docstring for SOC - Handles estimation of SOC based on start voltage and couloumb counting (current integration)
 	Input: dt (time in between interrupts), batteryCapacityInAh"""
 	soc = 0
 
-	def __init__(self, dt, battery_capacity_ah):
+	def __init__(self, dt, battery_capacity_ah, slope1, slope2, slope3, slope4, intercept1, intercept2, intercept3, intercept4):
 		self.dt = dt # Defined time in between interrupts
 		self.batteryCapacityInC = battery_capacity_ah*3600 # 1Ah = 3600 Coulombs
+		self.slope1 = slope1
+		self.slope2 = slope2
+		self.slope3 = slope3
+		self.slope4 = slope4
+		self.intercept1 = intercept1
+		self.intercept2 = intercept2
+		self.intercept3 = intercept3
+		self.intercept4 = intercept4
 
+
+	def estimate_ocv(self):
+		if 0 < self.soc < 9.9625:
+			ocv = self.soc * self.slope1 + self.intercept1
+		elif 9.9625 <= self.soc < 30.0875:
+			ocv = self.soc * self.slope2 + self.intercept2
+		elif 30.0875 <= self.soc < 75:
+			ocv = self.soc * self.slope3 + self.intercept3
+		elif 75 <= self.soc < 100:
+			ocv = self.soc * self.slope4 + self.intercept4
+		else:
+			pass
+
+		return ocv
 
 	def estimate_start_SOC(self, voltageReading): # ((JUST A TESTING FUNCTION - NOT DONE))
-		self.soc = voltageReading # Er SOC på grafen fra 0-1 skal der ganges med 100 her.
-		if voltageReading < xvalue1:
-			self.soc = voltageReading * slope1 + intercept1
-			return self.soc
-		elif xvalue1 <= voltageReading < xvalue2:
-			self.soc = voltageReading * slope2 + intercept2
-			return self.soc
-		elif xvalue2 <= voltageReading < xvalue3:
-			self.soc = voltageReading * slope3 + intercept3
-			return self.soc
-		elif xvalue3 <= voltageReading < xvalue4:
-			self.soc = voltageReading * slope4 + intercept4
-			return self.soc
+
+		if V1 < voltageReading < V2:
+			self.soc = voltageReading * self.slope1 + self.intercept1
+		elif V2 <= voltageReading < v3:
+			self.soc = voltageReading * self.slope2 + self.intercept2
+		elif V3 <= voltageReading < v4:
+			self.soc = voltageReading * self.slope3 + self.intercept3
+		elif V4 <= voltageReading < V5:
+			self.soc = voltageReading * self.slope4 + self.intercept4
+		else:
+			pass
+
+		return self.soc
+
 
 	def esitmate_continuous_SOC(self, currentReading):
 		self.soc = self.soc + (currentReading * self.dt * 100) / self.batteryCapacityInC # *100 fordi procent
 		return self.soc
+
+soc = SOCOCV(0.1, 18, 1,2,3,4,5,6,7,8)
+ocv = SOCOCV(0.1, 18, 1,2,3,4,5,6,7,8)
+
+soc.estimate_start_SOC()
